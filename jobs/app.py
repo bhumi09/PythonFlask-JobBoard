49d9db6db74882940be8ecfd500e9bde02d95fb4
bnandan <bhumi.n.2000@gmail.com>
2019-05-12 18:49:15 +0530

    dynamic content done

--- jobs/app.py
+++ jobs/app.py
@@ -23,7 +23,7 @@ def execute_sql(sql,values=(),commit = False,single=False):
     cursor.close()
     return result
 
-@app.tear_down_appcontext
+@app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g,'_connection',None)
     if connection is not None:
